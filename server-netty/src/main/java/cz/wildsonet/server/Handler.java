efef8961c43a78eca5c0db5afa5f14f758ab6d58
Marek Jelen <marek@jelen.biz>
2010-12-27 23:44:01 +0100

    More efficient large request handling

--- server-netty/src/main/java/cz/wildsonet/server/Handler.java
+++ server-netty/src/main/java/cz/wildsonet/server/Handler.java
@@ -8,6 +8,7 @@ import org.jboss.netty.util.CharsetUtil;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,75 +16,112 @@ public class Handler extends SimpleChannelUpstreamHandler {
 
     private final StringBuilder buf = new StringBuilder();
 
+    private File tempFile;
+    private FileOutputStream uploadStream;
+    private FileInputStream rackInput;
+
+    private Map<String, Object> env;
+
     public void messageReceived(ChannelHandlerContext context, MessageEvent event) throws Exception {
 
-        HttpRequest request = (HttpRequest) event.getMessage();
+        if (event.getMessage() instanceof HttpRequest) {
 
-        Map<String, Object> env = new HashMap<String, Object>();
+            HttpRequest request = (HttpRequest) event.getMessage();
 
-        int[] rack_version = {1, 1};
+            env = new HashMap<String, Object>();
 
-        String scheme = "http";
-        String hostname = HttpHeaders.getHost(request, "localhost");
-        String port = "80";
-        String queryString = "";
-        String pathInfo = "";
+            int[] rack_version = {1, 1};
 
-        if(request.containsHeader("X-For-Host")){
-            hostname = request.getHeader("X-For-Host");
+            String scheme = "http";
+            String hostname = HttpHeaders.getHost(request, "localhost");
+            String port = "80";
+            String queryString = "";
+            String pathInfo = "";
+
+            if (request.containsHeader("X-For-Host")) {
+                hostname = request.getHeader("X-For-Host");
+            }
+
+            if (request.containsHeader("X-For-Scheme")) {
+                scheme = request.getHeader("X-For-Scheme");
+            }
+
+            if (hostname.contains(":")) {
+                String temp[] = hostname.split(":");
+                hostname = temp[0];
+                port = temp[1];
+            }
+
+            if (request.getUri().contains("?")) {
+                String[] temp = request.getUri().split("\\?", 2);
+                pathInfo = temp[0];
+                queryString = temp[1];
+            }
+
+            request.removeHeader("Host");
+            if ((!port.equals("80") && !scheme.equals("http")) && (!port.equals("443") && !scheme.equals("https"))) {
+                request.addHeader("Host", hostname + ":" + port);
+            } else {
+                request.addHeader("Host", hostname);
+            }
+
+            tempFile = File.createTempFile(request.getMethod().getName() + ".", ".request", new File("tmp"));
+            uploadStream = new FileOutputStream(tempFile);
+
+            if (!request.isChunked()) {
+                request.getContent().readBytes(uploadStream, request.getContent().capacity());
+                uploadStream.close();
+            }
+
+            rackInput = new FileInputStream(tempFile);
+
+            env.put("REQUEST_METHOD", request.getMethod().getName());
+            env.put("SCRIPT_NAME", "");
+            env.put("PATH_INFO", pathInfo);
+            env.put("QUERY_STRING", queryString);
+            env.put("SERVER_NAME", hostname);
+            env.put("SERVER_PORT", port);
+
+            env.put("rack.version", rack_version);
+            env.put("rack.url_scheme", scheme);
+            env.put("rack.input", rackInput);
+            env.put("rack.errors", System.err);
+            env.put("rack.multithread", true);
+            env.put("rack.multiprocess", false);
+            env.put("rack.run_once", false);
+
+            env.put("wsn.context", context);
+
+            for (String header : request.getHeaderNames()) {
+                env.put("HTTP_" + header.replaceAll("-", "_").toUpperCase(), request.getHeader(header));
+            }
+
+            for (String en : env.keySet()) {
+                buf.append("ENV: ").append(en).append(": \t").append(env.get(en).toString()).append("\r\n");
+            }
+
+            buf.append("THREAD: ").append(Thread.currentThread().getName());
+
+            if(!request.isChunked()){
+                handleResponse(event);
+            }
+
+        } else {
+
+            HttpChunk chunk = (HttpChunk) event.getMessage();
+            chunk.getContent().readBytes(uploadStream, chunk.getContent().capacity());
+
+            if (chunk.isLast()) {
+
+                uploadStream.close();
+
+                handleResponse(event);
+
+            }
         }
+    }
 
-        if(request.containsHeader("X-For-Scheme")){
-            scheme = request.getHeader("X-For-Scheme");
-        }
-
-        if(hostname.contains(":")){
-            String temp[] = hostname.split(":");
-            hostname = temp[0];
-            port = temp[1];
-        }
-
-        if(request.getUri().contains("?")){
-            String[] temp = request.getUri().split("\\?", 2);
-            pathInfo = temp[0];
-            queryString = temp[1];
-        }
-
-        request.removeHeader("Host");
-        if((!port.equals("80") && !scheme.equals("http")) && (!port.equals("443") && !scheme.equals("https"))){
-            request.addHeader("Host", hostname + ":" + port);
-        }else{
-            request.addHeader("Host", hostname);
-        }
-
-        File tempFile = File.createTempFile(request.getMethod().getName() + ".", ".request", new File("tmp"));
-        FileOutputStream uploadStream = new FileOutputStream(tempFile);
-        request.getContent().readBytes(uploadStream, request.getContent().capacity());
-        uploadStream.close();
-
-        FileInputStream rackInput = new FileInputStream(tempFile);
-
-        env.put("REQUEST_METHOD", request.getMethod().getName());
-        env.put("SCRIPT_NAME", "");
-        env.put("PATH_INFO", pathInfo);
-        env.put("QUERY_STRING", queryString);
-        env.put("SERVER_NAME", hostname);
-        env.put("SERVER_PORT", port);
-
-        env.put("rack.version", rack_version);
-        env.put("rack.url_scheme", scheme);
-        env.put("rack.input", rackInput);
-        env.put("rack.errors", System.err);
-        env.put("rack.multithread", true);
-        env.put("rack.multiprocess", false);
-        env.put("rack.run_once", false);
-
-        env.put("wsn.request", request);
-        env.put("wsn.context", context);
-
-        for (String header : request.getHeaderNames()) {
-            env.put("HTTP_" + header.replaceAll("-", "_").toUpperCase(), request.getHeader(header));
-        }
+    private void handleResponse(MessageEvent event) throws IOException {
 
         HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
         response.setContent(ChannelBuffers.copiedBuffer(buf.toString(), CharsetUtil.UTF_8));
@@ -93,7 +131,8 @@ public class Handler extends SimpleChannelUpstreamHandler {
         future.addListener(ChannelFutureListener.CLOSE);
 
         rackInput.close();
-        tempFile.delete();
+        boolean deleted = tempFile.delete();
+
     }
 
 }
--- server-netty/src/main/java/cz/wildsonet/server/PipelineFactory.java
+++ server-netty/src/main/java/cz/wildsonet/server/PipelineFactory.java
@@ -5,8 +5,12 @@ import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+import org.jboss.netty.handler.execution.ExecutionHandler;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 
 public class PipelineFactory implements ChannelPipelineFactory {
+
+    private static ExecutionHandler executionHandler = new ExecutionHandler(new OrderedMemoryAwareThreadPoolExecutor(16, 0, 0));
 
     public ChannelPipeline getPipeline() throws Exception {
 
@@ -14,6 +18,8 @@ public class PipelineFactory implements ChannelPipelineFactory {
 
         pipeline.addLast("decoder", new HttpRequestDecoder());
         pipeline.addLast("encoder", new HttpResponseEncoder());
+
+        pipeline.addLast("executor", executionHandler);
 
         pipeline.addLast("handler", new Handler());
 
